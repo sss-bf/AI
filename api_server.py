6941d6ab59f5d0220661ff42ddf036e7b889446c
ybkimdev <ybkim.dev@gmail.com>
2025-02-26 22:43:36 +0900

    Add Test API

--- api_server.py
+++ api_server.py
@@ -4,13 +4,19 @@ from test_ai_model import AIModel
 
 app = FastAPI()
 
-@app.get("/api/v1/guide")
-def GetGuide(query: str, image_url: str):
+@app.post("/api/v1/guide")
+def GetGuide(user_request: str, image_url: str):
     guide_text: str = ""
     guide_image_url = ""
 
     ai_model = AIModel()
 
-    guide_text, guide_image_url = ai_model.request(query, image_url)
+    # TODO: 프롬프트 생성 필요
+    prompt = user_request
 
-    return {"guideText": guide_text, "guideImageUrl": guide_image_url}
+    # guide_text, guide_image_url = ai_model.request(prompt, image_url)
+    # guide_text, guide_image_url = ai_model.test_request(prompt), image_url
+
+    guide_text, guide_image_url = user_request, image_url
+
+    return {"guideText": f"AI Model Response : [{guide_text}]", "guideImageUrl": f"AI Model Response : [{image_url}]"}
--- test_ai_model.py
+++ test_ai_model.py
@@ -13,6 +13,22 @@ class AIModel:
         self.gpt_model = "gpt-4o"
         self.dalle_model = "dall-e-3"
     
+    def test_request(self, prompt):
+        gpt_response = self.client.chat.completions.create(
+            model = self.gpt_model,
+            messages = [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": prompt}
+                    ],
+                }
+            ],
+            max_tokens = 500
+        )
+        guide_text = gpt_response.choices[0].message.content
+        return guide_text
+
     def request(self, prompt, image_url):
         # Read file from URL
         response = requests.get(image_url)
@@ -52,9 +68,10 @@ class AIModel:
 
         return guide_text, guide_image_url
 
-
 # NOTE: (for Test)
 if __name__ == "__main__":
     model = AIModel()
-    guide_text, guide_image_url = model.request("이 사진에 대해 설명해줘", "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png")
-    print(guide_text, guide_image_url)
+    # guide_text, guide_image_url = model.request("이 사진에 대해 설명해줘", "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png")
+    # print(guide_text, guide_image_url)
+    guide_text = model.test_request("안녕하세요")
+    print(guide_text)
