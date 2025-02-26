05c7fbee357a4853b165f81cf5653541dc8ce11d
ybkimdev <ybkim.dev@gmail.com>
2025-02-26 16:42:26 +0900

    Add Template of guide API (GET Method)

--- api_server.py
+++ api_server.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get("/test")
 def read_root(item: str):
     return {"result": "item"}
+
+
+@app.get("/api/v1/guide")
+def GetGuide(query: str, image_url: str):
+    guide_text: str = ""
+    guide_image = None
+
+    # TODO: GPT API Call & result 생성 및 반환
+
+    return {"guideText": guide_text, "guideImage": guide_image}
